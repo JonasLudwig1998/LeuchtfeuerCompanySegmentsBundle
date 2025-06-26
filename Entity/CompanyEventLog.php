9713e07a1479f7f0abcbdf3b6d8b97ba4717a6ad
lenonleite <lenonleite@gmail.com>
2025-06-26 13:51:01 +0100

    - added table company_event_log - added tests - added Update trigger create table

---
+++ Entity/CompanyEventLog.php
@@ -0,0 +1,251 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Mautic\ApiBundle\Serializer\Driver\ApiMetadataDriver;
+use Mautic\CoreBundle\Doctrine\Mapping\ClassMetadataBuilder;
+use Mautic\LeadBundle\Entity\Company;
+
+/**
+ * Store here company events.
+ */
+class CompanyEventLog
+{
+    /**
+     * @var string
+     */
+    public const INDEX_SEARCH = 'IDX_SEARCH';
+
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var Company|null
+     */
+    protected $company;
+
+    /**
+     * @var int|null
+     */
+    protected $userId;
+
+    /**
+     * @var string|null
+     */
+    protected $userName;
+
+    /**
+     * @var string|null
+     */
+    protected $bundle;
+
+    /**
+     * @var string|null
+     */
+    protected $object;
+
+    /**
+     * @var int|null
+     */
+    protected $objectId;
+
+    /**
+     * @var string|null
+     */
+    protected $action;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected $dateAdded;
+
+    /**
+     * @var array|null
+     */
+    protected $properties;
+
+    public static function loadMetadata(ORM\ClassMetadata $metadata): void
+    {
+        $builder = new ClassMetadataBuilder($metadata);
+        $builder->setTable('company_event_log')
+            ->setCustomRepositoryClass(CompanyEventLogRepository::class)
+            ->addIndex(['company_id'], 'company_id_index')
+            ->addIndex(['object', 'object_id'], 'company_object_index')
+            ->addIndex(['bundle', 'object', 'action', 'object_id'], 'company_timeline_index')
+            ->addIndex(['bundle', 'object', 'action', 'object_id', 'date_added'], self::INDEX_SEARCH)
+            ->addIndex(['action'], 'company_timeline_action_index')
+            ->addIndex(['date_added'], 'company_date_added_index')
+            ->addBigIntIdField()
+            ->addNullableField('userId', Types::INTEGER, 'user_id')
+            ->addNullableField('userName', Types::STRING, 'user_name')
+            ->addNullableField('bundle', Types::STRING)
+            ->addNullableField('object', Types::STRING)
+            ->addNullableField('action', Types::STRING)
+            ->addNullableField('objectId', Types::INTEGER, 'object_id')
+            ->addNamedField('dateAdded', Types::DATETIME_MUTABLE, 'date_added')
+            ->addNullableField('properties', Types::JSON);
+
+        $builder->createManyToOne('company', Company::class)
+            ->addJoinColumn('company_id', 'id', true, false, 'CASCADE')
+            ->inversedBy('eventLog')
+            ->build();
+    }
+
+    public function __construct()
+    {
+        $this->setDateAdded(new \DateTime());
+    }
+
+    /**
+     * Prepare the metadata for API usage.
+     */
+    public static function loadApiMetadata(ApiMetadataDriver $metadata): void
+    {
+        $metadata->setGroupPrefix('import')
+            ->addListProperties([
+                'id',
+                'companyId',
+                'userId',
+                'userName',
+                'bundle',
+                'object',
+                'objectId',
+                'action',
+                'dateAdded',
+                'properties',
+            ])
+            ->build();
+    }
+
+    public function getId(): int
+    {
+        return (int) $this->id;
+    }
+
+    public function setCompany(Company $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(?int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->userName;
+    }
+
+    public function setUserName(?string $userName): self
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    public function getBundle(): ?string
+    {
+        return $this->bundle;
+    }
+
+    public function setBundle(?string $bundle): self
+    {
+        $this->bundle = $bundle;
+
+        return $this;
+    }
+
+    public function getObject(): ?string
+    {
+        return $this->object;
+    }
+
+    public function setObject(?string $object): self
+    {
+        $this->object = $object;
+
+        return $this;
+    }
+
+    public function getObjectId(): ?int
+    {
+        return $this->objectId;
+    }
+
+    public function setObjectId(?int $objectId): self
+    {
+        $this->objectId = $objectId;
+
+        return $this;
+    }
+
+    public function getAction(): ?string
+    {
+        return $this->action;
+    }
+
+    public function setAction(?string $action): self
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    public function getDateAdded(): ?\DateTime
+    {
+        return $this->dateAdded;
+    }
+
+    public function setDateAdded(?\DateTime $dateAdded): self
+    {
+        $this->dateAdded = $dateAdded;
+
+        return $this;
+    }
+
+    public function getProperties(): ?array
+    {
+        return $this->properties;
+    }
+
+    public function setProperties(?array $properties): self
+    {
+        $this->properties = $properties;
+
+        return $this;
+    }
+
+    /**
+     * Set one property into the properties array.
+     *
+     * @param string $key
+     * @param string $value
+     */
+    public function addProperty($key, $value): self
+    {
+        $this->properties[$key] = $value;
+
+        return $this;
+    }
+}
---
+++ Model/CompanyEventLogModel.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Model;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Mautic\CoreBundle\Helper\CoreParametersHelper;
+use Mautic\CoreBundle\Helper\UserHelper;
+use Mautic\CoreBundle\Model\FormModel;
+use Mautic\CoreBundle\Security\Permissions\CorePermissions;
+use Mautic\CoreBundle\Translation\Translator;
+use Mautic\LeadBundle\Entity\Company;
+use MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Entity\CompanyEventLog;
+use MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Entity\CompanySegment;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Entity\CompanyEventLogRepository;
+use Doctrine\ORM\EntityRepository;
+use Mautic\CoreBundle\Entity\CommonRepository;
+
+class CompanyEventLogModel extends FormModel
+{
+    public function __construct(
+        EntityManagerInterface $em,
+        CorePermissions $security,
+        EventDispatcherInterface $dispatcher,
+        UrlGeneratorInterface $router,
+        Translator $translator,
+        UserHelper $userHelper,
+        LoggerInterface $logger,
+        CoreParametersHelper $coreParametersHelper,
+    ) {
+        parent::__construct($em, $security, $dispatcher, $router, $translator, $userHelper, $logger, $coreParametersHelper);
+    }
+
+    public function getRepository(): CompanyEventLogRepository|EntityRepository|CommonRepository
+    {
+        return $this->em->getRepository(CompanyEventLog::class);
+    }
+
+    public function saveCompanyEventLog(string $action, Company $company, CompanySegment $companySegment): void
+    {
+        $companyEventLog = new CompanyEventLog();
+        $companyEventLog->setCompany($company);
+        $companyEventLog->setBundle('company');
+        $companyEventLog->setAction($action);
+        $companyEventLog->setObject('segment');
+        $companyEventLog->setObjectId($companySegment->getId());
+        $companyEventLog->setDateAdded(new \DateTime());
+        $userId      = null; // Set the user ID if available
+        $userName    = 'System'; // or use the actual user name if available
+        $currentUser = $this->userHelper->getUser();
+        if ($currentUser) {
+            $userId   = $currentUser->getId();
+            $userName = $currentUser->getUsername();
+        }
+        $companyEventLog->setProperties([
+            'company_segment_id'   => $companySegment->getId(),
+            'company_segment_name' => $companySegment->getName(),
+            'company_id'           => $company->getId(),
+            'object_description'   => $companySegment->getName(),
+        ]);
+        $companyEventLog->setUserId($userId); // Set the user ID if available
+        $companyEventLog->setUserName($userName); // or use the actual user name if available
+        $this->saveEntity($companyEventLog);
+    }
+}
