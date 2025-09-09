240c5547b605e7bdd3ac0d075b98a9614fef6ab7
lenonleite <lenonleite@gmail.com>
2025-09-09 13:32:37 +0100

    fix phpstan and fixcs

--- Tests/Functional/Controller/CompanySegmentControllerTest.php
+++ Tests/Functional/Controller/CompanySegmentControllerTest.php
@@ -116,6 +116,7 @@ class CompanySegmentControllerTest extends MauticMysqlTestCase
         $url                  = sprintf('/s/company-segments/batchDelete?tmpl=list&ids=["%s","%s"]', $companySegmentGlibi->getId(), $companySegmentTBS->getId());
         $this->client->request('POST', $url);
         self::assertIsString($this->client->getResponse()->getContent());
+        dd($this->client->getResponse()->getContent());
         self::assertStringContainsString('cannot be deleted, it is required by other segments.', $this->client->getResponse()->getContent());
         self::assertStringContainsString('cannot be deleted, it is required by other company segments.', $this->client->getResponse()->getContent());
         self::assertStringNotContainsString('has been deleted!', $this->client->getResponse()->getContent());
