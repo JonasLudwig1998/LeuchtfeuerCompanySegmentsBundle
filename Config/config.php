15fbd2109587652ed26edb4be13f5157493055f6
Ekkehard Gümbel <43146234+ekkeguembel@users.noreply.github.com>
2025-10-01 18:21:38 +0200

    Update version in config.php

--- Config/config.php
+++ Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Company Segments by Leuchtfeuer',
     'description' => 'Provide a 2nd type of Segments which can contain Companies (and allows segment filters).',
-    'version'     => '1.2.2',
+    'version'     => '1.2.4',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
     'routes'      => [
         'main' => [
